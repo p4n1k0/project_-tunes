a76a979c54dfb916cb20f0f262d3915a8e66e53f
p4n1k0 <gabre.tarick@gmail.com>
2025-05-08 18:29:56 -0300

    profileeditcss

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
 import { getUser, updateUser } from '../services/userAPI';
+import './ProfileEdit.css';
 
 const INITIAL_STATE = {
   loading: false,
@@ -67,69 +68,92 @@ class ProfileEdit extends Component {
     if (loading) return <Loading />
 
     return (
-      <div data-testid="page-profile-edit">
-        <section>
-          <Header />
-          <h1>Editar perfil</h1>
-          <form>
-            <label htmlFor="name-user">
-              Nome
-              <input
-                data-testid="edit-input-name"
-                type="text"
-                id="name-user"
-                name="name"
-                value={name}
-                onChange={this.changeProfile}
-              />
-            </label>
-            <br />
-            <label htmlFor="email-user">
-              E-mail
-              <input
-                data-testid="edit-input-email"
-                type="email"
-                id="email-user"
-                name="email"
-                value={email}
-                onChange={this.changeProfile}
-              />
-              <br />
-            </label>
-            <label htmlFor="description-user">
-              Descrição
-              <textarea
-                data-testid="edit-input-description"
-                id="description-user"
-                name="description"
-                value={description}
-                onChange={this.changeProfile}
-              />
-              <br />
-            </label>
-            <label htmlFor="image-user">
-              Alterar Imagem
+      <form className="user-tools-edit-container">
+        <Header />
+        <span className="user-tools-edit-box">
+          {
+            image === ''
+              ? (
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+                  alt="foto padrao de pessoa usuaria"
+                />
+              )
+              : (
+                <img
+                  src={image}
+                  alt="foto de perfil da pessoa usuaria"
+                />
+              )
+          }
+          <span className="user-image-options">
+            <h2>Editar foto do perfil</h2>
+            <label htmlFor="image" className="image-input-label">
+              Cole aqui a URL da imagem:
               <input
                 data-testid="edit-input-image"
                 type="text"
-                id="image-user"
+                id="image"
                 name="image"
                 value={image}
                 onChange={this.changeProfile}
+                className="image-input"
               />
               <br />
             </label>
-            <button
-              data-testid="edit-button-save"
-              type="button"
-              disabled={isDisabled}
-              onClick={this.saveUser}
-            >
-              Editar perfil
-            </button>
-          </form>
-        </section>
-      </div>
+            <p>Ou escolha uma foto do seu computador</p>
+            <label htmlFor="inter-image" className="intern-image-input-label">
+              Enviar arquivo
+              <input
+                name="intern-image"
+                id="inter-image"
+                type="file"
+                onChange={this.changeProfile}
+                className="itnern-image-input"
+              />
+            </label>
+          </span>
+        </span>
+        <span className="user-info-edit-box">
+          <h2>Nome</h2>
+          <input
+            data-testid="edit-input-name"
+            type="text"
+            id="name-user"
+            name="name"
+            value={name}
+            onChange={this.changeProfile}
+            className="edit-input-name"
+          />
+          <h2>E-mail</h2>
+          <span className="user-email-box">
+            <input
+              data-testid="edit-input-email"
+              type="email"
+              id="email-user"
+              name="email"
+              value={email}
+              onChange={this.changeProfile}
+            />
+          </span>
+          <h2>Descrição</h2>
+          <textarea
+            data-testid="edit-input-description"
+            id="description-user"
+            name="description"
+            value={description}
+            onChange={this.changeProfile}
+          />
+          <button
+            data-testid="edit-button-save"
+            type="submit"
+            disabled={isDisabled}
+            onClick={this.saveUser}
+          >
+            Salvar
+          </button>
+        </span>
+      </form>
     );
   };
 };
